1c21374b37f661583d4b81879e6825cda1da8596
Madray Haven <sgpublic2002@gmail.com>
2025-07-12 09:09:42 +0800

    chore: migrate to pixi

--- hwdc_trainer/upload.py
+++ hwdc_trainer/upload.py
@@ -1,17 +1,9 @@
 from hwdc_trainer.hwdc_model_trainer import HwdcModelTrainer
 
-
-def start_train():
-    trainer = HwdcModelTrainer()
-    trainer.load(use_pretrained=False)
-    trainer.train()
-
-
 def upload():
     trainer = HwdcModelTrainer()
     trainer.load(use_pretrained=False)
     trainer.upload()
 
-
-if __name__ == "__main__":
-    start_train()
+if __name__ == '__main__':
+    upload()
